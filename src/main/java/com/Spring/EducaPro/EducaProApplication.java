ce38905aca4751c090424fbfef470fe2c015b1a9
Lucas Barbosa Barreto <lucasbarbosabarreto551@gmail.com>
2025-12-11 22:30:21 -0300

    Initial commit

---
+++ src/main/java/com/Spring/EducaPro/EducaProApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.EducaPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring/EducaPro/EducaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.EducaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
